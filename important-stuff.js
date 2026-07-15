6b5d93e8fe171049c46eb43501537ff9085ff90c
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-07-15 14:50:59 -0400

    Added array of two characters, a heading and button.

---
+++ important-stuff.js
@@ -0,0 +1,20 @@
+const characters = [{
+    name: "knuckles",
+    hints: [
+        "First appearence was in 1994",
+        "Favorite food is grapes",
+        "Treasure hunts on occasion",
+        "Has red locs"
+    ]
+},
+{
+    name: "tails",
+    hints: [
+        "Can fly and carry teamates",
+        "Has a fear of lightning",
+        "Is a genuis inventor",
+        "Real name is Miles Prower"
+    ]
+}
+];
+
