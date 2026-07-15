703a9e4e6cb37d4b0b56957da42561d2489d7371
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-07-15 16:14:00 -0400

    Moved stuff in the index.html to the body where it's supposed to go. Added game logic to important-stuff.js

--- important-stuff.js
+++ important-stuff.js
@@ -15,6 +15,25 @@ const characters = [{
         "Is a genuis inventor",
         "Real name is Miles Prower"
     ]
+},
+{
+    name: "sonic",
+    hints: [
+        "Born on Christmas Island",
+        "Was originally designed as a rabbit",
+        "Had a cameo in a racing game before his official debut",
+        "Eyes switched to green design around the early 2000's"
+    ]
+}
+    ,
+{
+    name: "amy",
+    hints: [
+        "Debuted in the game 'Sonic CD'",
+        "Used to wear an orange skirt in original design",
+        "Freed Sonic from Prison Island",
+        "Main weapon is the Piko Piko Hammer"
+    ]
 }
 ];
 
@@ -22,7 +41,51 @@ const statusEl = document.getElementById("status");
 const hintListEl = document.getElementById("hint-list");
 const guessingbutton = document.getElementById("guessing-button");
 
-function pickRandomCharacter(){
-    const index = Math.floor(Math.random() * characters.length);
-    return characters [i];
+function pickRandomCharacter() {
+    const i = Math.floor(Math.random() * characters.length);
+    return characters[i];
 }
+
+let secretCharacter = pickRandomCharacter();
+let visableHints = 0;
+let gameOver = false;
+
+
+function showHint() {
+    const li = document.createElement("li")
+    li.textContent = secretCharacter.hints[visableHints];
+    hintListEl.appendChild(li);
+    visableHints++;
+}
+
+function updateStatus() {
+    statusEl.textContent = `You have ${secretCharacter.hints.length - visableHints} guesses left`;
+}
+guessingbutton.addEventListener("click", handleGuess);
+
+function handleGuess() {
+    if (gameOver) return;
+
+    const raw = prompt("Who am I?");
+    if (raw == null) return;
+
+    const guess = raw.trim().toLowerCase();
+
+    if (guess === secretCharacter.name) {
+        alert("You guessed it")
+        gameOver = true;
+        return;
+    }
+
+    if (visableHints >= secretCharacter.hints.length) {
+        alert(`Sorry! Out of guesses.. the answer was ${secretCharacter.name}.`);
+        gameOver = true;
+        return;
+    }
+
+    alert("Nah, that is not the answer. Here's another hint");
+    showHint();
+    updateStatus();
+}
+showHint();
+updateStatus();
